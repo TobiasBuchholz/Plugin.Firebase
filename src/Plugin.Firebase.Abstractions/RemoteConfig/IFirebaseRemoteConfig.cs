d138d1f80f89cb8840d3fafe504136fc88c9790e
Tobias Buchholz <tobias.buchholz@pressmatrix.de>
2018-08-28 11:58:04 +0200

    add support for Firebase RemoteConfig

---
+++ src/Plugin.Firebase.Abstractions/RemoteConfig/IFirebaseRemoteConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Plugin.Firebase.Abstractions.RemoteConfig
+{
+    public interface IFirebaseRemoteConfig : IDisposable
+    {
+        void SetDefaults(IDictionary<string, object> defaults);
+        void SetDefaults(params (string, object)[] defaults);
+        void SetDeveloperModeEnabled(bool isEnabled);
+        Task FetchAsync(double expirationDuration = 0);
+        void ActivateFetched();
+        bool GetBoolean(string key);
+        string GetString(string key);
+        long GetLong(string key);
+        double GetDouble(string key);
+        byte[] GetByteArray(string key);
+    }
+}

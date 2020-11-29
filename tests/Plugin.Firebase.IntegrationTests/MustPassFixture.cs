1e3b122f36266c68fa65bdeca0f907cfee2aa17d
Tobias Buchholz <tobias.buchholz@pressmatrix.de>
2020-11-29 20:37:20 +0100

    add ios integration test project

---
+++ tests/Plugin.Firebase.IntegrationTests/MustPassFixture.cs
@@ -0,0 +1,29 @@
+using System;
+using Plugin.Firebase.Analytics;
+using Plugin.Firebase.Auth;
+using Plugin.Firebase.CloudMessaging;
+using Plugin.Firebase.DynamicLinks;
+using Plugin.Firebase.Firestore;
+using Plugin.Firebase.Functions;
+using Plugin.Firebase.RemoteConfig;
+using Plugin.Firebase.Storage;
+using Xunit;
+
+namespace Plugin.Firebase.IntegrationTests
+{
+    public class MustPassFixture
+    {
+        [Fact]
+        public void must_pass()
+        {
+            Assert.True(CrossFirebaseAnalytics.IsSupported);
+            Assert.True(CrossFirebaseAuth.IsSupported);
+            Assert.True(CrossFirebaseCloudMessaging.IsSupported);
+            Assert.True(CrossFirebaseDynamicLinks.IsSupported);
+            Assert.True(CrossFirebaseFirestore.IsSupported);
+            Assert.True(CrossFirebaseFunctions.IsSupported);
+            Assert.True(CrossFirebaseStorage.IsSupported);
+            Assert.True(CrossFirebaseRemoteConfig.IsSupported);
+        }
+    }
+}
---
+++ tests/Plugin.Firebase.TestHarness.iOS/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UIKit;
+
+namespace Plugin.Firebase.TestHarness.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "IntegrationTestAppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "IntegrationTestAppDelegate");
+        }
+    }
+}

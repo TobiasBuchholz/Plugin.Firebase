28929c9d9c5161ca533b2bffcab510cfabe9a9e1
kapusch <myvanity.development@gmail.com>
2022-05-15 21:06:37 +0200

    Apply formatting rules

--- tests/Plugin.Firebase.IntegrationTests/Storage/StorageFixture.cs
+++ tests/Plugin.Firebase.IntegrationTests/Storage/StorageFixture.cs
@@ -84,7 +84,7 @@ namespace Plugin.Firebase.IntegrationTests.Storage
         private static void AssertDownloadUrl(string pathToFile, string downloadUrl)
         {
             var port = DeviceInfo.Platform == DevicePlatform.iOS ? ":443" : "";
-            
+
             Assert.StartsWith(
                 $"https://firebasestorage.googleapis.com{port}/v0/b/pluginfirebase-integrationtest.appspot.com/o/{pathToFile}?alt=media&token=",
                 WebUtility.UrlDecode(downloadUrl));

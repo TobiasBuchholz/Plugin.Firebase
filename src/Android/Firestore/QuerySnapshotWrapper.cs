f8d72854fee03ff439bf3b83a8123aa61a9ea57f
Tobias Buchholz <tobias.buchholz89@gmail.com>
2022-12-18 21:40:32 +0100

    Enable IDocumentSnapshot<T> in firestore DocumentChange

--- src/Android/Firestore/QuerySnapshotWrapper.cs
+++ src/Android/Firestore/QuerySnapshotWrapper.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Firebase.Firestore;
 using Plugin.Firebase.Firestore;
-using DocumentChange = Plugin.Firebase.Firestore.DocumentChange;
 
 namespace Plugin.Firebase.Android.Firestore
 {
@@ -15,16 +14,16 @@ namespace Plugin.Firebase.Android.Firestore
             _wrapped = querySnapshot;
         }
 
-        public IEnumerable<DocumentChange> GetDocumentChanges(bool includeMetadataChanges)
+        public IEnumerable<DocumentChange<T>> GetDocumentChanges(bool includeMetadataChanges)
         {
             return _wrapped
                 .GetDocumentChanges(includeMetadataChanges ? MetadataChanges.Include : MetadataChanges.Exclude)
-                .Select(x => x.ToAbstract());
+                .Select(x => x.ToAbstract<T>());
         }
 
         public IEnumerable<IDocumentSnapshot<T>> Documents => _wrapped.Documents.Select(x => x.ToAbstract<T>());
         public ISnapshotMetadata Metadata => _wrapped.Metadata.ToAbstract();
-        public IEnumerable<DocumentChange> DocumentChanges => _wrapped.DocumentChanges.Select(x => x.ToAbstract());
+        public IEnumerable<DocumentChange<T>> DocumentChanges => _wrapped.DocumentChanges.Select(x => x.ToAbstract<T>());
         public IQuery Query => _wrapped.Query.ToAbstract();
         public bool IsEmpty => _wrapped.IsEmpty;
         public int Count => _wrapped.Size();
--- src/iOS/Firestore/QuerySnapshotWrapper.cs
+++ src/iOS/Firestore/QuerySnapshotWrapper.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Firebase.CloudFirestore;
 using Plugin.Firebase.Firestore;
-using DocumentChange = Plugin.Firebase.Firestore.DocumentChange;
 
 namespace Plugin.Firebase.iOS.Firestore
 {
@@ -15,16 +14,16 @@ namespace Plugin.Firebase.iOS.Firestore
             _wrapped = querySnapshot;
         }
 
-        public IEnumerable<DocumentChange> GetDocumentChanges(bool includeMetadataChanges)
+        public IEnumerable<DocumentChange<T>> GetDocumentChanges(bool includeMetadataChanges)
         {
             return _wrapped
                 .GetDocumentChanges(includeMetadataChanges)
-                .Select(x => x.ToAbstract());
+                .Select(x => x.ToAbstract<T>());
         }
 
         public IEnumerable<IDocumentSnapshot<T>> Documents => _wrapped.Documents.Select(x => x.ToAbstract<T>());
         public ISnapshotMetadata Metadata => _wrapped.Metadata.ToAbstract();
-        public IEnumerable<DocumentChange> DocumentChanges => _wrapped.DocumentChanges.Select(x => x.ToAbstract());
+        public IEnumerable<DocumentChange<T>> DocumentChanges => _wrapped.DocumentChanges.Select(x => x.ToAbstract<T>());
         public IQuery Query => _wrapped.Query.ToAbstract();
         public bool IsEmpty => _wrapped.IsEmpty;
         public int Count => (int) _wrapped.Count;

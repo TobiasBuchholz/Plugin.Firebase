b3eeae268734f93a162e7eb7c17e657eb6d2fc1c
Rob <robertmaxted1994@gmail.com>
2024-05-01 20:19:48 +0100

    fixed issues.

--- src/Functions/Platforms/iOS/FirebaseFunctionsImplementation.cs
+++ src/Functions/Platforms/iOS/FirebaseFunctionsImplementation.cs
@@ -7,19 +7,24 @@ namespace Plugin.Firebase.Functions;
 public sealed class FirebaseFunctionsImplementation : DisposableBase, IFirebaseFunctions
 {
     private readonly CloudFunctions _functions;
+    private readonly string _region; // Store the region
 
     public FirebaseFunctionsImplementation(string region = "us-central1") // Default to "us-central1" if not specified
     {
-        _functions = CloudFunctions.GetInstance(region);
+        _functions = CloudFunctions.SharedInstance;
+        _region = region; // Initialize with specified region
     }
 
     public IHttpsCallable GetHttpsCallable(string name)
     {
-        return new HttpsCallableWrapper(_functions.HttpsCallable(name));
+        // Specify the region for each function
+        var function = _functions.GetHttpsCallable(name);
+        function.Region = _region;
+        return new HttpsCallableWrapper(function);
     }
 
     public void UseEmulator(string host, int port)
     {
-        _functions.UseEmulatorOriginWithHost(host, (uint) port);
+        _functions.UseFunctionsEmulatorOrigin($"{host}:{port}");
     }
 }
--- src/Functions/Shared/CrossFirebaseFunctions.cs
+++ src/Functions/Shared/CrossFirebaseFunctions.cs
@@ -4,7 +4,6 @@ public sealed class CrossFirebaseFunctions
 {
     private static Lazy<IFirebaseFunctions> _implementation = new Lazy<IFirebaseFunctions>(() => CreateInstance("us-central1"), System.Threading.LazyThreadSafetyMode.PublicationOnly);
 
-    // Overload that allows setting the region
     public static void Initialize(string region)
     {
         _implementation = new Lazy<IFirebaseFunctions>(() => CreateInstance(region), System.Threading.LazyThreadSafetyMode.PublicationOnly);
@@ -19,14 +18,8 @@ public sealed class CrossFirebaseFunctions
 #endif
     }
 
-    /// <summary>
-    /// Gets if the plugin is supported on the current platform.
-    /// </summary>
     public static bool IsSupported => _implementation.Value != null;
 
-    /// <summary>
-    /// Current plugin implementation to use
-    /// </summary>
     public static IFirebaseFunctions Current {
         get {
             var ret = _implementation.Value;
@@ -40,9 +33,6 @@ public sealed class CrossFirebaseFunctions
     private static Exception NotImplementedInReferenceAssembly() =>
         new NotImplementedException("This functionality is not implemented in the portable version of this assembly. You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
 
-    /// <summary>
-    /// Dispose of everything 
-    /// </summary>
     public static void Dispose()
     {
         if(_implementation != null && _implementation.IsValueCreated) {

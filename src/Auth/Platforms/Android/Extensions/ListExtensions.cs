85fe657d93791a07092fb8adb7bede23975d1f23
Adam Essenmacher <adam.essenmacher@gmail.com>
2026-05-02 14:18:43 -0400

    Fix auth token nested claims

--- src/Auth/Platforms/Android/Extensions/ListExtensions.cs
+++ src/Auth/Platforms/Android/Extensions/ListExtensions.cs
@@ -5,21 +5,22 @@ namespace Plugin.Firebase.Auth.Platforms.Android.Extensions;
 
 public static class ListExtensions
 {
+    public static IList ToList(this Java.Util.IList @this, Type? targetType = null)
+    {
+        var list = CreateList(targetType);
+        for(var i = 0; i < @this.Size(); i++) {
+            AddConvertedValue(list, @this.Get(i), targetType);
+        }
+        return list;
+    }
+
     public static IList ToList(this JavaList @this, Type? targetType = null)
     {
-        var list =
-            targetType == null
-                ? new List<object>()
-                : (IList) Activator.CreateInstance(typeof(List<>).MakeGenericType(targetType))!;
+        var list = CreateList(targetType);
         for(var i = 0; i < @this.Size(); i++) {
-            var value = @this[i];
-            if(value is Java.Lang.Object javaValue) {
-                list!.Add(javaValue.ToObject(targetType));
-            } else {
-                list!.Add(value);
-            }
+            AddConvertedValue(list, @this[i], targetType);
         }
-        return list!;
+        return list;
     }
 
     public static JavaList ToJavaList(this IEnumerable @this)
@@ -29,5 +30,21 @@ public static class ListExtensions
             list.Add(item.ToJavaObject());
         }
         return list;
+    }
+
+    private static IList CreateList(Type? targetType)
+    {
+        return targetType == null
+            ? new List<object?>()
+            : (IList) Activator.CreateInstance(typeof(List<>).MakeGenericType(targetType))!;
+    }
+
+    private static void AddConvertedValue(IList list, object? value, Type? targetType)
+    {
+        if(value is Java.Lang.Object javaValue) {
+            list.Add(javaValue.ToObject(targetType));
+        } else {
+            list.Add(value);
+        }
     }
 }
